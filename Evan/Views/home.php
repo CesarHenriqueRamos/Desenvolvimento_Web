a9293ccac1100321db2eade9682fbfbc86c3328d
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-04-09 16:02:14 -0300

    novas alteraçoes

---
+++ Evan/Views/home.php
@@ -0,0 +1,20 @@
+<?php
+    include('header.php')
+?>
+        
+                <nav class="menu">
+                    <ul>
+                        <li><a href="login.php">Logar</a></li>
+                        <li><a href="">Cadastrar</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <section></section>
+        
+        <footer class="footer2">
+            <h3>Todos Os Direitos Reservados</h3>
+        </footer>    
+</body>
+</html>
