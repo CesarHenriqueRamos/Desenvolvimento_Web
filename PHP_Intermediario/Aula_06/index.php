59d4cb7dabf245bd5e0fb92f4aeb0e4e51f08dd9
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-04-10 12:00:59 -0300

    resolução do problema iframe

--- PHP_Intermediario/Aula_06/index.php
+++ PHP_Intermediario/Aula_06/index.php
@@ -9,4 +9,7 @@ session_destroy();
 //cookie
 setcookie('nome','Cesar Henrique Ramos Cookie',time()+(60*60),'/');
 echo $_COOKIE['nome'];
+//resolução do problema da plataforma Evan
 ?>
+
+<iframe width="420" height="315" src="https://www.youtube.com/embed/N1eB6C0zWDE" frameborder="0" allowfullscreen></iframe>
