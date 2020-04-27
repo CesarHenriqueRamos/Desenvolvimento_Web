d0d29b6ea4bd343a1dfebc9eaacc5220acf5849f
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-04-27 11:55:02 -0300

    delete

--- PHP_Banco_de_Dado/aula_02/index.php
+++ PHP_Banco_de_Dado/aula_02/index.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Sao_Paulo');
 $pdo = new PDO('mysql:host=localhost;dbname=modulo_8','root','');
 //delete
-$id = 9;
+$id = 11;
   
     $sql = $pdo->prepare('DELETE FROM `clientes` WHERE id=?');
     $sql->execute(array($id));
