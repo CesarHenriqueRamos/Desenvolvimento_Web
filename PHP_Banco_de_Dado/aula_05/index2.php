4b9338f8486370f35a6db2ce8a4ee9967a1e884d
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-04-29 12:00:16 -0300

    atualisação

--- PHP_Banco_de_Dado/aula_05/index2.php
+++ PHP_Banco_de_Dado/aula_05/index2.php
@@ -1,16 +1,14 @@
 <?php
 date_default_timezone_set('America/Sao_Paulo');
 $pdo = new PDO('mysql:host=localhost;dbname=db_teste','root','');
+//sql query
 //lock tables
-$sql = $pdo->prepare("LOCK TABLEs `cargos` WRITE");
-
-$sql->execute();
-$dados = $sql->fetchAll();
-foreach($dados as $key => $value){
-    echo $value['nome'];
-    echo '<br>';
-    echo $value['nome_cargos'];
-    echo '<hr>';
-}
+//$sql = $pdo->prepare("LOCK TABLEs `cargos` WRITE");
+//LIKE otimo para pesquisar com 
+//$sql = $pdo->prepare("SELECT * FROM `cargos` WHERE email LIKE '%@g%'");
+//IN doas id 1 e 10
+$sql = $pdo->prepare("SELECT * FROM `cargos` WHERE id IN (1,10)");
+//BETWEEN otimo para datas, para analisar um intervalo de data 
+$sql = $pdo->prepare("SELECT * FROM `cargos` WHERE data BETWEEN '2020-01-01' AND '2020-02-01'");
 
 ?>
