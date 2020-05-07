c89fea523b203ad7ae367de1d999eb8eecd7afa6
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-05-07 16:25:43 -0300

    atualisação

--- Criando_Site_Dinâmico/config.php
+++ Criando_Site_Dinâmico/config.php
@@ -1,3 +1,7 @@
 <?php
     define('INCLUDE_PATH', 'http://localhost/Meus_Projetos/Desenvolvimento_Web/Desenvolvimento_Web/Criando_Site_Din%C3%A2mico/');
+    $autoload = function($class){
+        echo $class;
+    };
+    spl_autoload_extensions($autoload);
 ?>
