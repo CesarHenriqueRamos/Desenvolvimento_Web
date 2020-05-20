f175d27b3274992c7b06b6d0250212e416e1a4a5
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-05-20 11:59:56 -0300

    correções

--- Criando_Site_Dinâmico/painel/main.php
+++ Criando_Site_Dinâmico/painel/main.php
@@ -28,37 +28,6 @@
 		<div class="clear"></div><!--clear-->
 		</div><!--container-->
     </header>
-    <div class="menu">
-        <div class="menu-wraper">
-            <div class="box-usuario">
-            <?php if($_SESSION['img'] == ''){?>
-            <div class="avata-user"><i class="fa fa-user"></i></div>
-            <?php } else{?>
-                <div class="img-user"><img src="<?php INCLUDE_PATH_PAINEL?>uploads/<?php echo $_SESSION['img'];?>" alt="">
-            <?php }?>    
-                <p id="bem-vindo"><?php echo $_SESSION['nome'] ?></p>
-                <p id="cargo"><?php echo pegaCargo($_SESSION['cargo']);?></p>
-                <hr>
-               
-        </div><!--boo-usuario-->
-        <div class="items-menu">
-                <h2>Cadastro</h2>
-                    <a href="">Cadastro de Depoimento</a>
-                    <a href="">Cadastro de Serviço</a>
-                    <a href="">Cadastro de Slides</a>
-                <h2>Gestão</h2>
-                    <a href="">Lista de Depoimento</a>
-                    <a href="">Lista de Serviço</a>
-                    <a href="">Lista de Slides</a>
-                <h2>Administrador</h2>
-                    <a href="">Editar Usuario</a>
-                    <a href="">Adcionar Usuario</a>
-                <h2>Configuração Geral</h2>
-                    <a href="">Editar</a>
-        </div><!--itens-menu-->
-        </div>     
-        
-    </div>
     <div class="conteudo">
         <div class="container">
             
@@ -84,6 +53,7 @@
             <div class="box-container w100"></div>
             
         </div>
+        
         <?php
             /*for($i = 0; $i <10000; $i++){
                 echo 'Teste<br>';
@@ -92,6 +62,39 @@
         
         <div class="clear"></div>
     </div>
+    <div class="menu">
+        <div class="menu-wraper">
+            <div class="box-usuario">
+                <?php if($_SESSION['img'] == ''){?>
+                <div class="avata-user"><i class="fa fa-user"></i></div>
+                <?php } else{?>
+                    <div class="img-user"><img src="<?php INCLUDE_PATH_PAINEL?>uploads/<?php echo $_SESSION['img'];?>" alt="">
+                <?php }?>    
+                    <p id="bem-vindo"><?php echo $_SESSION['nome'] ?></p>
+                    <p id="cargo"><?php echo pegaCargo($_SESSION['cargo']);?></p>
+                    <hr>
+               
+            </div><!--boo-usuario-->
+            <div class="items-menu">
+                    <h2>Cadastro</h2>
+                        <a href="">Cadastro de Depoimento</a>
+                        <a href="">Cadastro de Serviço</a>
+                        <a href="">Cadastro de Slides</a>
+                    <h2>Gestão</h2>
+                        <a href="">Lista de Depoimento</a>
+                        <a href="">Lista de Serviço</a>
+                        <a href="">Lista de Slides</a>
+                    <h2>Administrador</h2>
+                        <a href="">Editar Usuario</a>
+                        <a href="">Adcionar Usuario</a>
+                    <h2>Configuração Geral</h2>
+                        <a href="">Editar</a>
+            </div><!--itens-menu-->
+        </div>     
+        <div class="clear"></div>
+    </div>
+    
+    
 
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
