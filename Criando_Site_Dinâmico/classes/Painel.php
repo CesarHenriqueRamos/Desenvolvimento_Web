219197ca6701f232e644a11cee969fa2540a330e
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-05-21 19:44:48 -0300

    correção de bug

--- Criando_Site_Dinâmico/classes/Painel.php
+++ Criando_Site_Dinâmico/classes/Painel.php
@@ -29,5 +29,33 @@ class Painel{
             echo '<div class="err"> <i class="fas fa-times"></i> '.$mesagem.'</div>';
         }
     }
+    public static function imagemValida($imagem){
+        if($imagem['type'] == 'image/jpeg' || $imagem['type'] == 'image/jpg' || $imagem['type'] == 'image/png'){
+            $tamanho = intval($imagem['size']/1024);
+            if($tamanho < 300){
+                return true;
+            }else{
+                return false;
+            }            
+        }else{
+            return false;
+        }
+    }
+   
+    public static function uploadFile($file){
+        $formatoArquivo = explode('.',$file['name']);
+        $imagemNome = uniqid().'.'.$formatoArquivo[count($formatoArquivo) - 1];
+        if(move_uploaded_file($file['tmp_name'],BASE_DIR_PAINEL.'/uploads/'.$imagemNome)){
+            return $imagemNome;
+        }else{
+            return false;
+        }
+     }
+    public static function deleteFile($file){
+        @unlink('uploads/'.$file);
+    }
+    
+
+    
 }
 ?>
