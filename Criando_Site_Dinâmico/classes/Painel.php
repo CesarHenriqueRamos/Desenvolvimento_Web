4a428830ccc7b633984eebf47100296172bf0590
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-05-25 16:55:25 -0300

    atualisação

--- Criando_Site_Dinâmico/classes/Painel.php
+++ Criando_Site_Dinâmico/classes/Painel.php
@@ -98,7 +98,7 @@ class Painel{
        }
        return $certo;
    }
-
+  
     
 }
 ?>
--- Criando_Site_Dinâmico/painel/pages/listar-depoimento.php
+++ Criando_Site_Dinâmico/painel/pages/listar-depoimento.php
@@ -1,3 +1,8 @@
+<?php
+$sql = MySql::connect()->prepare("SELECT * FROM `tb_site.depoimentos`");
+$sql->execute();
+$depoimenstos = $sql->fetchAll();
+?>
 <div class="box-container w100">
     <h2 class="title"><i class="far fa-list-alt"></i> Listar Depoimentos</h2>
     <hr>
@@ -6,10 +11,13 @@
             <td>Nome:</td>
             <td>Depoimento:</td>
         </tr>
+        <?php
+        foreach($depoimenstos as $key => $value){?>
         <tr>
-            <td>Cesar</td>
-            <td>Teste</td>
+            <td><?php echo $value['nome'];?></td>
+            <td><?php echo $value['depoimento'];?></td>
         </tr>
+        <?php } ?>
         <div class="clear"></div>
     </table>
 </div>
