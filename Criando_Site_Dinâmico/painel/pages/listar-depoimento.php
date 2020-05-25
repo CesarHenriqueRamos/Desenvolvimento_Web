3fd87870c26f8a6f2c3cf909cace12c2bf3a9923
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-05-25 16:39:03 -0300

    atualisação

--- Criando_Site_Dinâmico/painel/pages/listar-depoimento.php
+++ Criando_Site_Dinâmico/painel/pages/listar-depoimento.php
@@ -1,1 +1,15 @@
-teste
+<div class="box-container w100">
+    <h2 class="title"><i class="far fa-list-alt"></i> Listar Depoimentos</h2>
+    <hr>
+    <table>
+        <tr>
+            <td>Nome:</td>
+            <td>Depoimento:</td>
+        </tr>
+        <tr>
+            <td>Cesar</td>
+            <td>Teste</td>
+        </tr>
+        <div class="clear"></div>
+    </table>
+</div>
