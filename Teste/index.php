18453386fe20ebe3af0feea618432ef8c7ee683e
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-01 16:12:36 -0300

    a

--- Teste/index.php
+++ Teste/index.php
@@ -2,7 +2,7 @@
 if(file_exists('file.txt')){
     echo 'o arquivo existe';
 }else{
-    $conteiner = 'Novo Arquivo';
+    $conteiner = "Novo Arquivo \r\n outra linha\r\noutra linha";
     file_put_contents('file.txt',$conteiner);
 }
 ?>
