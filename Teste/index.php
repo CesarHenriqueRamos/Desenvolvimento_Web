ec537f11fde5faabd15a31bfecc78761943818f3
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-01 16:33:43 -0300

    diretorios

--- Teste/index.php
+++ Teste/index.php
@@ -1,8 +1,18 @@
 <?php
-if(file_exists('file.txt')){
-    echo 'o arquivo existe';
+/*if(file_exists('file.txt')){
+    echo 'o arquivo existe<br>';
 }else{
+    //para quebrar linha \r\n
     $conteiner = "Novo Arquivo \r\n outra linha\r\noutra linha";
+    //para escrever um arquivo
     file_put_contents('file.txt',$conteiner);
-}
+} 
+//para ler um arquivo    
+$conteudo = file_get_contents('file.txt');
+echo nl2br($conteudo);
+//função para deletear arquivos
+//unlink('file.txt');
+*/
+//criar pasta
+mkdir('pasta');
 ?>
