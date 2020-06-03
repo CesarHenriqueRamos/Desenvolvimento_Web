b01db24efaf8622e5c98c948b7c6ee150722e459
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-03 15:39:05 -0300

    03-06-2020

--- Portal_de_Noticia/painel/pages/editar-noticias.php
+++ Portal_de_Noticia/painel/pages/editar-noticias.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * to do
- * fazer a pagina de editar noticias
- */
     if(isset($_GET['id'])){
         $id = intval($_GET['id']);
         $dados = Painel::select("tb_site.noticias", 'id = ?',$id);
@@ -15,34 +11,58 @@
 <div class="box-container w100">
     <h2 class="title"><i class="far fa-edit"></i> Editar Depoimento</h2>
     <hr>
-
-    <form action="" method="post" enctype="multipart/form-data" id="editar-usuario">
  <?php
     if(isset($_POST['acao'])){
-        if(Painel::update($_POST)){
-           Painel::alert('sucesso', 'Depoimento Editado com Sucesso');
-           $dados = Painel::select("tb_site.servico", 'id = ?',$id);
+        //enviado o formulario
+        $titulo = $_POST['titulo'];
+        $imagem = $_FILES['imagem'];
+        $imagem_atual = $_POST['imagem_atual'];
+        $conteudo = $_POST['conteudo'];
+        $categoria_id = $_POST['categoria_id'];
+        $order_id = $_POST['order_id'];
+        $id = $_GET['id'];
+
+        //validação
+        if($titulo == ''){
+            Painel::alert('erro', 'É Necessário Preencher o Campo Titulo');
+        }else if($conteudo == ''){
+            Painel::alert('erro', 'É Necessário Preencher o Campo Conteudo');
+        }else if(Painel::imagemValida($imagem) == false){
+            $imagem = $imagem_atual;
+            $arr = ['categoria_id'=>$categoria_id,'titulo'=>$titulo,'conteudo'=>$conteudo,'capa'=>$imagem,'order_id'=>$order_id,'nome_tabela'=>'tb_site.noticias','id'=>$id];
+            Painel::update($arr);
+            Painel::alert('sucesso', 'Cadastrado com Sucesso');
         }else{
-            Painel::alert('erro', 'Ocorreu um Erro ao Editado'); 
+            //função cadastra no banco de dados os dado
+            $imagem = Painel::uploadFile($imagem);
+            Painel::deleteFile($imagem_atual);
+            $arr = ['categoria_id'=>$categoria_id,'titulo'=>$titulo,'conteudo'=>$conteudo,'capa'=>$imagem,'order_id'=>$order_id,'nome_tabela'=>'tb_site.noticias','id'=>$id];
+            Painel::update($arr);
+            Painel::alert('sucesso', 'Cadastrado com Sucesso');
         }
-        
     }
  ?>
- <?php
+    <form action="" method="post" enctype="multipart/form-data" id="editar-usuario">
+
     
-    ?>
+   
         <div class="box-form">
             <label for="nome">Nome:</label>
-            <input type="text" name="servico" id="nome" value="<?php echo $dados['servico'] ?>">
+            <input type="text" name="titulo" id="nome" value="<?php echo $dados['titulo'] ?>">
         </div>
         <div class="box-form">
             <label for="mensagem">Mensagem:</label>
-            <textarea name="descricao" id="mensagem" cols="30" rows="10" ><?php echo $dados['descricao'] ?></textarea>
+            <textarea name="conteudo" id="conteudo" cols="30" rows="10" ><?php echo $dados['conteudo'] ?></textarea>
         </div>
-        
+        <div class="box-form">
+            <label for="img">Imagem:</label>
+            <input type="file" name="imagem">
+            <input type="hidden" name="imagem_atual" value="<?php echo $dados['capa'] ?>">
+        </div>
         <div class="box-form">
             <input type="hidden" name="id" value="<?php echo $dados['id']; ?>"> 
-            <input type="hidden" name="nome_tabela" value="tb_site.servico">        
+            <input type="hidden" name="order_id" value="<?php echo $dados['order_id']; ?>"> 
+            <input type="hidden" name="categoria_id" value="<?php echo $dados['categoria_id']; ?>">        
             <input type="submit" name="acao" value="Editar">
         </div>
     </form>
