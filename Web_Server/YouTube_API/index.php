091a59aa251e75c8443896e78ac99d4ce6769cf8
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-12 18:09:54 -0300

    Atualisado

--- Web_Server/YouTube_API/index.php
+++ Web_Server/YouTube_API/index.php
@@ -1,7 +1,6 @@
 <?php
 //Chave da Api:AIzaSyAunbV6v_cyfNblfVSY-erDwzAnp8oeA5o
 require 'vendor/autoload.php';
-$youtube = new Madcoda\Youtube\Youtube(array('key' => 'AIzaSyAunbV6v_cyfNblfVSY-erDwzAnp8oeA5o'));
-$video = $youtube->getVideoInfo('rie-hPVJ7Sw');
-print_r($video);
+//$youtube = new Madcoda\Youtube\Youtube(array('key' => 'AIzaSyAunbV6v_cyfNblfVSY-erDwzAnp8oeA5o'));
+//$video = $youtube->getVideoInfo('rie-hPVJ7Sw');
 ?>
