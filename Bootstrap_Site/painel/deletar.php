ab16dbd78b2556fc39eb0571ca45462ef8bc4d07
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-18 17:03:38 -0300

    Atualisação

--- Bootstrap_Site/painel/deletar.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if(isset($_POST['id_membro'])){
-	 $pdo = new PDO('mysql:host=localhost;dbname=saite_bootstrap','root','');
-	 $sql = $pdo->prepare("DELETE FROM `equipe` WHERE id = ?");
-	 $sql->execute(array($_POST['id_membro']));
-	}
-?>
