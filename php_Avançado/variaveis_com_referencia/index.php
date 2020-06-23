545eec25ab4d9bd9ad6ab582e2a4444df6ccb844
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-06-23 18:13:12 -0300

    Ultima Atualisação do dia

--- php_Avançado/variaveis_com_referencia/index.php
+++ php_Avançado/variaveis_com_referencia/index.php
@@ -1,3 +1,32 @@
 <?php
+/*$nome = 'Cesar';
+function teste(&$nome){
+    $nome = 'Henrique';
+}
+teste($nome);
+echo $nome;*/
+/*$nome = 'Cesar';
+$teste = &$nome;
+echo $teste;
+echo '<br>';
+echo $nome;
+echo '<br>';
+$teste = 'Henrique';
+echo $teste;
+echo '<br>';
+echo $nome;*/
 
+//operador Ternario
+/*$nome = "Cesar";
+$mensagem = 'Ola '.(isset($nome)? $nome: 'Visitante');
+echo $mensagem;*/
+//null coalescing operator
+/*$valor = $_POST['acao'] ?? $_POST['opcao'] ?? 'Nada Deu Certo :(';
+echo $valor;*/
+//Nullable types
+function teste() :?string{
+    return 9;
+}
+$numero = teste();
+var_dump($numero);
 ?>
